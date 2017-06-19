318282a40a708f493fd6a6cb70f4b5623cc7f449
Jose Salazar <jose.salazar.home@gmail.com>
2017-06-18 18:08:09 -0700

    Added Spotify functionality

--- assets/js/liri.js
+++ assets/js/liri.js
@@ -1,8 +1,7 @@
 var Twitter = require('twitter');
 var inquirer = require("inquirer");
+var Spotify = require('node-spotify-api');
 var keys = require("./keys");
-
-var inquirer = inquirerResponse;
 
 var client = new Twitter ({
 	consumer_key: keys.twitterKeys.consumer_key,
@@ -11,19 +10,38 @@ var client = new Twitter ({
 	access_token_secret: keys.twitterKeys.access_token_secret
 });
 
+var spotify = new Spotify({
+  id: "35563073063e418aa3673cc30523cc50",
+  secret: "f568229cd07c483d99097e09d419310e"
+});
+
 function postTweet(msg) {
 	client.post('statuses/update', {status: msg}, function(error, tweet, response) {
 	  if (!error) {
-	    console.log(tweet);
+	    console.log("You posted: " + tweet.text + ". View all tweets at https://twitter.com/thereal_party!");
 	  }
+	});
+
+	client.get('statuses/user_timeline', {screen_name: 'thereal_party'}, function(error, tweet, response) {
+		if(!error) {
+			console.log("Your last few tweets:")
+			for (let i = 0; i < 4; i++) {
+				console.log(tweet[i].text + " - " + tweet[i].created_at);
+			}
+		}
 	});
 }
 
-function retrieveTweet() {
-	client.get('statuses/user_timeline', {screen_name: 'thereal_party'}, function(error, tweet, response) {
-		if(!error) {
-			console.log(tweet[0].text);
-		}
+function spotifySearch(song) {
+	spotify.search({ type: 'track', query: song }, function(err, data) {
+	  if (err) {
+	    return console.log('Error occurred: ' + err);
+	  }
+	  	
+	 	console.log("Artist: " + data.tracks.items[0].artists[0].name);
+	 	console.log("Song: " + data.tracks.items[0].name);
+	 	console.log("Listen to it here: " + data.tracks.items[0].preview_url);
+		console.log("Album: " + data.tracks.items[0].album.name); 
 	});
 }
 
@@ -32,19 +50,29 @@ inquirer
   .prompt([
     // Here we create a basic password-protected text prompt.
     {
+      type: "input",
+      message: "Hi! My name is Leery! The simplest and most perverted bot! What is your name?",
+      name: "name"
+    },
+    {
       type: "list",
       message: "What do you want to do?",
-      choices: ["Tweet Stuff", "View Tweets", "Nothing"],
+      choices: ["Tweet Stuff", "Listen to Tunes", "Look up a movie", "Nothing"],
       name: "toDo"
-    }
-    if (inquirerResponse.toDo === "Tweet Stuff") {
+    },
+    {
     	type: "input",
-    	message: "What would you like to tweet?",
-    	name: "tweet"
+    	message: "Type in a value!",
+    	name: "value"
     }
   ])
   .then(function(inquirerResponse) {
-    if (inquirerResponse.toDo === "Tweet Stuff") {
-    	postTweet();
+  	var inquirer = inquirerResponse;
+    if (inquirer.toDo === "Tweet Stuff") {
+    	postTweet(inquirer.value);
+    } else if (inquirer.toDo === "Listen to Tunes") {
+		spotifySearch(inquirer.value);
+    } else {
+    	console.log("Well... here's something you to look at: ( o )( o )");
     }
   });
